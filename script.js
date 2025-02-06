c8b4e3cc122679b8115f0f7c907b279360156f2d
ShaneBrown10 <shanebrown10@hotmail.com>
2025-02-06 20:39:47 +0000

    new branch

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 const OPENWEATHER_API_KEY = "a1e0f54713be1d1d848f352296f8aaa7";
-const DAYS_OF_THE_WEEK = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
+<<<<<<< HEAD
+        function getLatitudeLongitude(placeName, onSuccess, onError) {
+            const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${placeName}&limit=3&appid=${OPENWEATHER_API_KEY}`;
+
+            fetch(apiUrl)
+                .then((resp) => {
+                    if(!resp.ok) {
+                        onError("couldn't fetch weather information");
+                        return;
+                    }
+                    return resp.json();
+                }).then((data) => {
+                    if (!data || data.length === 0) {
+                        onError("no data");
+                        return;
+                    }
+
+                    onSuccess({ lon: data[0].lon, lat: data[0].lat });
+                }).catch((err) => onError(err));
+        }
+
+        /*
+            geolocation - An object with floating point "lon" and "lat" values.
+        */
+        function getWeatherInfo(geolocation, onSuccess, onError, useCelsius = true) {
+            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${geolocation.lat}&lon=${geolocation.lon}&units=${useCelsius ? "metric" : "imperial"}&appid=${OPENWEATHER_API_KEY}`
+            fetch(apiUrl)
+                .then((resp) => {
+                    if(!resp.ok) {
+                        onError("couldn't fetch weather information");
+                        return;
+                    }
+                    return resp.json();
+                }).then((data) => {
+                    onSuccess({ city: data.name, temp: data.main.temp, description: data.weather[0].description });
+                }).catch((err) => onError(err));
+        }
+=======
 function displayWeatherFor(placeName, onSuccess, onError, useCelsius = true) {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${placeName}&limit=3&appid=${OPENWEATHER_API_KEY}`)
         .then((resp) => {
@@ -22,94 +59,45 @@ function displayWeatherFor(placeName, onSuccess, onError, useCelsius = true) {
             onSuccess({ city: data.name, temp: data.main.temp, description: data.weather[0].description });
         }).catch((err) => onError(err));
 }
+>>>>>>> a1675c047420687cfe5d714ea7b1da4a025a1a15
 
-function displayFiveDayForecast(placeName, onError, useCelsius = true) {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${placeName}&limit=3&appid=${OPENWEATHER_API_KEY}`)
-        .then((resp) => {
-            if(!resp || !resp.ok) {
-                return Promise.reject("Could not get a response from the API.");
-            }
-            return resp.json();
-        }).then((data) => {
-            if (!data || data.length === 0) {
-                return Promise.reject(`Could not find a place called "${placeName}"`);
-            }
-            return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&units=${useCelsius ? "metric" : "imperial"}&appid=${OPENWEATHER_API_KEY}`);
-        }).then((resp) => {
-            return resp.json()
-        }).then((data) => {
-            const noonTemps = data.list.filter(x => x.dt_txt.includes("12:00"));
-    
-            forecastEls.day1.dotw.innerText = `${DAYS_OF_THE_WEEK[new Date(noonTemps[0].dt * 1000).getDay()]}`;
-            forecastEls.day2.dotw.innerText = `${DAYS_OF_THE_WEEK[new Date(noonTemps[1].dt * 1000).getDay()]}`;
-            forecastEls.day3.dotw.innerText = `${DAYS_OF_THE_WEEK[new Date(noonTemps[2].dt * 1000).getDay()]}`;
-            forecastEls.day4.dotw.innerText = `${DAYS_OF_THE_WEEK[new Date(noonTemps[3].dt * 1000).getDay()]}`;
-            forecastEls.day5.dotw.innerText = `${DAYS_OF_THE_WEEK[new Date(noonTemps[4].dt * 1000).getDay()]}`;
-    
-            forecastEls.day1.temp.innerText = `${noonTemps[0].main.temp.toFixed(1)}°C`;
-            forecastEls.day2.temp.innerText = `${noonTemps[1].main.temp.toFixed(1)}°C`;
-            forecastEls.day3.temp.innerText = `${noonTemps[2].main.temp.toFixed(1)}°C`;
-            forecastEls.day4.temp.innerText = `${noonTemps[3].main.temp.toFixed(1)}°C`;
-            forecastEls.day5.temp.innerText = `${noonTemps[4].main.temp.toFixed(1)}°C`;
-    
-            forecastEls.day1.weather.innerText = `${noonTemps[0].weather[0].main}`;
-            forecastEls.day2.weather.innerText = `${noonTemps[1].weather[0].main}`;
-            forecastEls.day3.weather.innerText = `${noonTemps[2].weather[0].main}`;
-            forecastEls.day4.weather.innerText = `${noonTemps[3].weather[0].main}`;
-            forecastEls.day5.weather.innerText = `${noonTemps[4].weather[0].main}`;
+        const searchBarEl = document.getElementById("search-bar");
+        const temperatureEl = document.getElementById("current-weather");
+        const weatherDescriptionEl = document.getElementById("weather-description");
+        const errorBanner = document.getElementById("error-banner");
 
-            carouselEl.style.visibility = "visible";
-        }).catch(() => {
-            carouselEl.style.visibility = "hidden";
+<<<<<<< HEAD
+        searchBarEl.addEventListener("change", function() {
+            getLatitudeLongitude(this.value, function(geolocation) {
+                getWeatherInfo(geolocation, function(data) {
+                    searchBarEl.value = data.city;
+                    temperatureEl.innerText = `Temperature: ${data.temp}°C`;
+                    weatherDescriptionEl.innerText = `Weather: ${data.description}`;
+                }, function(errorMessage) {
+                    errorBanner.style.display = 'block';
+                    errorBanner.innerText = `Error: ${errorMessage}`;
+                });
+            }, function(errorMessage) {
+                errorBanner.style.display = 'block';
+                errorBanner.innerText = `Error: ${errorMessage}`;
+            });
         });
-}
 
-const searchBarEl = document.getElementById("search-bar");
-const temperatureEl = document.getElementById("current-weather");
-const weatherDescriptionEl = document.getElementById("weather-description");
-const carouselEl = document.getElementById("carousel");
+        window.onerror = function(message, source, lineno, colno, error) {
+            errorBanner.style.display = 'block';
+            errorBanner.textContent = `Error: ${message} at ${source}:${lineno}:${colno}`;
+            return true; 
+        };
+        function closeErrorBanner() {
+            errorBanner.style.display = 'none';
+        };
 
-// Cache the element references for easier access.
-const forecastEls = {
-    day1: {
-        dotw: document.querySelector("#day1 .dotw"),
-        temp: document.querySelector("#day1 .temp"),
-        weather: document.querySelector("#day1 .weather"),
-    },
-    day2: {
-        dotw: document.querySelector("#day2 .dotw"),
-        temp: document.querySelector("#day2 .temp"),
-        weather: document.querySelector("#day2 .weather"),
-    },
-    day3: {
-        dotw: document.querySelector("#day3 .dotw"),
-        temp: document.querySelector("#day3 .temp"),
-        weather: document.querySelector("#day3 .weather"),
-    },
-    day4: {
-        dotw: document.querySelector("#day4 .dotw"),
-        temp: document.querySelector("#day4 .temp"),
-        weather: document.querySelector("#day4 .weather"),
-    },
-    day5: {
-        dotw: document.querySelector("#day5 .dotw"),
-        temp: document.querySelector("#day5 .temp"),
-        weather: document.querySelector("#day5 .weather"),
-    },
-};
-
+        errorBanner.addEventListener("click", closeErrorBanner);
+=======
 searchBarEl.addEventListener("change", function() {
     displayWeatherFor(this.value, function(data) {
         searchBarEl.value = data.city;
         temperatureEl.innerText = `${data.temp.toFixed(1)}°C`;
         weatherDescriptionEl.innerText = `${data.description}`;
-    }, function(err) {
-        temperatureEl.innerText = `--°C`;
-        weatherDescriptionEl.innerText = `--`;
-        window.alert(err);
-    },);
-    displayFiveDayForecast(this.value);
+    }, window.alert);
 });
-
-// Hide the carousel on page load
-carouselEl.style.visibility = "hidden";
