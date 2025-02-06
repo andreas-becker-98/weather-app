4e98d5bbf0f4fbbd5a11d04abc0a69136e4f226f
Andreas Becker <andreas-becker-98@users.noreply.github.com>
2025-02-06 18:37:44 +0000

    Cleaned up DOM manipulation and fixed error pathways for API calls.

--- script.js
+++ script.js
@@ -3,25 +3,18 @@ const OPENWEATHER_API_KEY = "a1e0f54713be1d1d848f352296f8aaa7";
 function displayWeatherFor(placeName, onSuccess, onError, useCelsius = true) {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${placeName}&limit=3&appid=${OPENWEATHER_API_KEY}`)
         .then((resp) => {
-            if(!resp) {
-                onError("aahhhhh");
-                return;
-            }
-            if(!resp.ok) {
-                onError("couldn't fetch weather information");
-                return;
+            if(!resp || !resp.ok) {
+                return Promise.reject("Could not get a response from the API.");
             }
             return resp.json();
         }).then((data) => {
             if (!data || data.length === 0) {
-                onError("no data");
-                return;
+                return Promise.reject(`Could not find a place called "${placeName}"`);
             }
             return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&units=${useCelsius ? "metric" : "imperial"}&appid=${OPENWEATHER_API_KEY}`);
         }).then((resp) => {
             if(!resp.ok) {
-                onError("couldn't fetch weather information");
-                return;
+                return Promise.reject("Couldn't fetch weather information");
             }
             return resp.json();
         }).then((data) => {
@@ -36,7 +29,7 @@ const weatherDescriptionEl = document.getElementById("weather-description");
 searchBarEl.addEventListener("change", function() {
     displayWeatherFor(this.value, function(data) {
         searchBarEl.value = data.city;
-        temperatureEl.innerText = `Temperature: ${data.temp}°C`;
-        weatherDescriptionEl.innerText = `Weather: ${data.description}`;
+        temperatureEl.innerText = `${data.temp.toFixed(1)}°C`;
+        weatherDescriptionEl.innerText = `${data.description}`;
     }, window.alert);
 });
